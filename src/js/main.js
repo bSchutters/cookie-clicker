9801b640b09c77f6e9c59b3b90da769fd054c2f7
bSchutters <bryan021@hotmail.be>
2023-10-27 16:07:31 +0200

    fix conflicts

--- src/js/main.js
+++ src/js/main.js
@@ -126,6 +126,12 @@ codyImage.addEventListener("click", () => {
   updateCostColors();
 });
 
+<<<<<<< HEAD
+=======
+const btnInfoRules = document.getElementById("info-rules");
+const btnInfoRulesMobile = document.getElementById("info-rules-mobile");
+
+>>>>>>> ca479aa (conflicts fix)
 btnStartModal.onclick = function () {
   overlayStartModal.classList.add("hidden");
   startModal.classList.add("hidden");
