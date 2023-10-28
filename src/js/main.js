8c6ca07b4cbf933a864ed99c14bf9c0cc33fa144
Alexander Fejari <alexander.fejari@gmail.com>
2023-10-28 09:13:47 +0200

    fix localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,21 @@
 import "../css/style.css";
 import "./store.js"; // Importez le store
 import {
-  getScore,
-  getClicValue,
-  setClicValue,
-  subscribe,
-  updateState,
-  scoreElement,
-  codyImage,
-  userName,
-  btnStartModal,
-  overlayStartModal,
-  startModal,
-  notif,
+    getScore,
+    getClicValue,
+    setClicValue,
+    subscribe,
+    updateState,
+    updateCostColors,
+    scoreElement,
+    codyImage,
+    userName,
+    btnStartModal,
+    overlayStartModal,
+    startModal,
+    notif,
+    rankLvlElements,
+    costElements
 } from "./store.js";
 
 import "./lvl1.js"; // Importez le niveau 1
@@ -29,13 +32,15 @@ import "./lvl11.js"; // Importez le niveau 11
 // ... Importez d'autres niveaux si nécessaire ...
 
 // app.js
+// Modifiez la valeur de clic
+setClicValue(1);
 
 //Composant qui garde getUserName
 function getUserName() {
-  const user = userName.value;
+    const user = userName.value;
 
-  // Stocker le username dans le localStorage
-  localStorage.setItem("userName", user);
+    // Stocker le username dans le localStorage
+    localStorage.setItem("userName", user);
 }
 
 // Abonnez le composant à l'état
@@ -44,17 +49,17 @@ subscribe(getUserName);
 // Récupérer username depuis le localStorage s'il existe
 const storeUser = localStorage.getItem("userName");
 if (storeUser !== null) {
-  updateState(userName);
+    updateState(userName);
 } else {
-  updateState("???");
+    updateState("???");
 }
 
 // Composant qui affiche et garde le score
 function scoreComponent(state) {
-  scoreElement.textContent = state.score;
+    scoreElement.textContent = state.score;
 
-  // Stocker le score dans le localStorage
-  localStorage.setItem("score", state.score.toString());
+    // Stocker le score dans le localStorage
+    localStorage.setItem("score", state.score.toString());
 }
 
 // Abonnez le composant à l'état
@@ -64,32 +69,31 @@ const storedScore = localStorage.getItem("score");
 
 // Récupérer le score depuis le localStorage s'il existe
 if (storedScore !== null) {
-  updateState({ score: parseInt(storedScore, 10) });
+    updateState({score: parseInt(storedScore, 10)});
 } else {
-  // Si le score n'existe pas dans le localStorage, initialisez-le
-  updateState({ score: 10 });
+    // Si le score n'existe pas dans le localStorage, initialisez-le
+    updateState({score: 10});
 }
 
 // Composant qui affiche et garde les bonus
 function getLvlAndCostValues() {
-  // Créez un objet pour stocker les valeurs
-  const valuesToStore = {};
+    // Créez un objet pour stocker les valeurs
+    const valuesToStore = {
+        clicValue: getClicValue()
+    };
 
-  const rankLvlElements = document.getElementsByClassName("rank");
-  const costElements = document.getElementsByClassName("cost");
+    for (let i = 0; i < rankLvlElements.length; i++) {
+        const rankValue = rankLvlElements[i].textContent;
+        const costValue = costElements[i].textContent;
+        valuesToStore[`rank${i}`] = rankValue;
+        valuesToStore[`cost${i}`] = costValue;
+    }
 
-  for (let i = 0; i < rankLvlElements.length; i++) {
-    const rankValue = rankLvlElements[i].textContent;
-    const costValue = costElements[i].textContent;
-    valuesToStore[`rank${i}`] = rankValue;
-    valuesToStore[`cost${i}`] = costValue;
-  }
+    // Convertissez l'objet en une chaîne JSON pour le stocker dans le localStorage
+    const serializedValues = JSON.stringify(valuesToStore);
 
-  // Convertissez l'objet en une chaîne JSON pour le stocker dans le localStorage
-  const serializedValues = JSON.stringify(valuesToStore);
-
-  // Enregistrez la chaîne JSON dans le localStorage
-  localStorage.setItem("rankLvlCostValues", serializedValues);
+    // Enregistrez la chaîne JSON dans le localStorage
+    localStorage.setItem("rankLvlCostValues", serializedValues);
 }
 
 // Abonnez le composant à l'état
@@ -98,83 +102,54 @@ subscribe(getLvlAndCostValues);
 // Récupérez la chaîne JSON du localStorage
 const storedData = localStorage.getItem("rankLvlCostValues");
 
-// Récupérez la chaîne JSON du localStorage
-if (storedData !== null) {
-  // 1 Désérialisez la chaîne JSON en un objet JavaScript
-  const rankLvlCostValues = JSON.parse(storedData);
-
-  // 2 Utilisez l'objet pour mettre à jour les éléments HTML correspondants
-  const rankLvlElements = document.getElementsByClassName("rank");
-  const costElements = document.getElementsByClassName("cost");
-
-  for (let i = 0; i < rankLvlElements.length; i++) {
-    if (rankLvlCostValues[`rank${i}`]) {
-      rankLvlElements[i].textContent = rankLvlCostValues[`rank${i}`];
-    }
-    if (rankLvlCostValues[`cost${i}`]) {
-      costElements[i].textContent = rankLvlCostValues[`cost${i}`];
-    }
-  }
-}
-
-// Abonnez le composant à l'état
-subscribe(scoreComponent);
-
-// Récupérer le score depuis le localStorage s'il existe
-if (storedScore !== null) {
-  updateState({ score: parseInt(storedScore, 10) });
-} else {
-  // Si le score n'existe pas dans le localStorage, initialisez-le
-  updateState({ score: 10 });
-}
+// Récupérez les éléments HTML correspondants
 
 if (storedData !== null) {
-  // 1 Désérialisez la chaîne JSON en un objet JavaScript
-  const rankLvlCostValues = JSON.parse(storedData);
+    // 1 Désérialisez la chaîne JSON en un objet JavaScript
+    const rankLvlCostValues = JSON.parse(storedData);
 
-  // 2 Utilisez l'objet pour mettre à jour les éléments HTML correspondants
-  const rankLvlElements = document.getElementsByClassName("rank");
-  const costElements = document.getElementsByClassName("cost");
-
-  for (let i = 0; i < rankLvlElements.length; i++) {
-    if (rankLvlCostValues[`rank${i}`]) {
-      rankLvlElements[i].textContent = rankLvlCostValues[`rank${i}`];
+    // 2 Utilisez l'objet pour mettre à jour les éléments HTML correspondants
+    for (let i = 0; i < rankLvlElements.length; i++) {
+        if (rankLvlCostValues[`rank${i}`]) {
+            rankLvlElements[i].textContent = rankLvlCostValues[`rank${i}`];
+        }
+        if (rankLvlCostValues[`cost${i}`]) {
+            costElements[i].textContent = rankLvlCostValues[`cost${i}`];
+        }
     }
-    if (rankLvlCostValues[`cost${i}`]) {
-      costElements[i].textContent = rankLvlCostValues[`cost${i}`];
+    // 3 Mettez à jour le clicValue
+    if (rankLvlCostValues.clicValue) {
+        setClicValue(rankLvlCostValues.clicValue);
     }
-  }
 }
 
-// Modifiez la valeur de clic
-setClicValue(1);
 
 codyImage.addEventListener("click", () => {
-  const currentScore = getScore();
-  const currentClicValue = getClicValue();
-  updateState({ score: currentScore + currentClicValue });
-  updateCostColors();
+    const currentScore = getScore();
+    const currentClicValue = getClicValue();
+    updateState({score: currentScore + currentClicValue});
+    updateCostColors();
 });
 
 const btnInfoRules = document.getElementById("info-rules");
 const btnInfoRulesMobile = document.getElementById("info-rules-mobile");
 
 btnStartModal.onclick = function () {
-  overlayStartModal.classList.add("hidden");
-  startModal.classList.add("hidden");
+    overlayStartModal.classList.add("hidden");
+    startModal.classList.add("hidden");
 };
 
 btnInfoRules.onclick = function () {
-  overlayStartModal.classList.remove("hidden");
-  startModal.classList.remove("hidden");
+    overlayStartModal.classList.remove("hidden");
+    startModal.classList.remove("hidden");
 };
 
 btnInfoRulesMobile.onclick = function () {
-  startModal.classList.remove("hidden");
+    startModal.classList.remove("hidden");
 };
 
 notif.onclick = function () {
-  notif.classList.add("hidden");
+    notif.classList.add("hidden");
 };
 
 //MENU BURGER OPEN CLOSE
@@ -185,17 +160,17 @@ const leftMain = document.getElementById("left-main");
 const closeLvlMenuBtn = document.getElementById("close-lvl-menu");
 
 iconBurgerMenu.onclick = function () {
-  rightMain.classList.add("hidden");
-  rightMain.classList.remove("flex");
-  leftMain.classList.remove("hidden");
-  leftMain.classList.add("flex");
-  closeLvlMenuBtn.classList.remove("hidden");
+    rightMain.classList.add("hidden");
+    rightMain.classList.remove("flex");
+    leftMain.classList.remove("hidden");
+    leftMain.classList.add("flex");
+    closeLvlMenuBtn.classList.remove("hidden");
 };
 
 closeLvlMenuBtn.onclick = function () {
-  rightMain.classList.add("flex");
-  rightMain.classList.remove("hidden");
-  leftMain.classList.remove("flex");
-  leftMain.classList.add("hidden");
-  closeLvlMenuBtn.classList.add("hidden");
+    rightMain.classList.add("flex");
+    rightMain.classList.remove("hidden");
+    leftMain.classList.remove("flex");
+    leftMain.classList.add("hidden");
+    closeLvlMenuBtn.classList.add("hidden");
 };
--- src/js/store.js
+++ src/js/store.js
@@ -4,6 +4,8 @@
 export const scoreElement = document.getElementById("score");
 export const codyImage = document.getElementById("cody");
 export const userName = document.getElementById("start-modal-input-username");
+export const rankLvlElements = document.getElementsByClassName("rank");
+export const costElements = document.getElementsByClassName("cost");
 export const startModal = document.getElementById("start-modal");
 export const btnStartModal = document.getElementById("btn-start-modal");
 export const overlayStartModal = document.getElementById("overlay");
@@ -16,13 +18,13 @@ export let autoClickerActive = false;
 export let autoClickValue = 1;
 
 export function setAutoClickValue(newValue) {
-  autoClickValue = newValue;
+    autoClickValue = newValue;
 }
 
 // Définition du store
 const state = {
-  score: 0,
-  clicValue: 1,
+    score: 0,
+    clicValue: 1,
 };
 
 // Tableau d'observateurs
@@ -30,106 +32,111 @@ const observers = [];
 
 // Fonction pour mettre à jour l'état et notifier les observateurs
 export function updateState(newState) {
-  Object.assign(state, newState);
-  notifyObservers();
-  updateCostColors();
+    Object.assign(state, newState);
+    notifyObservers();
+    updateCostColors();
 }
 
 // Fonction pour inscrire un observateur
 export function subscribe(observer) {
-  observers.push(observer);
+    observers.push(observer);
 }
 
 // Fonction pour notifier tous les observateurs
 export function notifyObservers() {
-  for (const observer of observers) {
-    observer(state);
-  }
+    for (const observer of observers) {
+        observer(state);
+    }
 }
 
 // Exportez uniquement les fonctions nécessaires pour lire et mettre à jour l'état
 export function getScore() {
-  return state.score;
+    return state.score;
 }
 
 export function getClicValue() {
-  return state.clicValue;
+    return state.clicValue;
 }
 
 export function setClicValue(newValue) {
-  updateState({ clicValue: newValue });
+    updateState({clicValue: newValue});
 }
 
 // Fonction pour activer l'autoclicker
 export function activateAutoClicker() {
-  autoClickerActive = true;
+    autoClickerActive = true;
 }
 
 // Fonction pour désactiver l'autoclicker
 export function deactivateAutoClicker() {
-  autoClickerActive = false;
-  ATRate.innerText = "False";
+    autoClickerActive = false;
+    ATRate.innerText = "False";
 }
+
 export function txtAT(text) {
-  ATRate.innerText = text;
+    ATRate.innerText = text;
 }
+
 export function getCostById(id) {
-  const item = itemsToWatch.find((item) => item.id === id);
-  if (item && item.cost) {
-    return item.cost();
-  }
-  return null;
-}
-export const itemsToWatch = [
-  { id: "cost-pizza", cost: null },
-  { id: "cost-burger", cost: null },
-  { id: "cost-coffee", cost: null },
-  { id: "cost-dorayaki", cost: null },
-  { id: "cost-fries", cost: null },
-  { id: "cost-HotDog", cost: null },
-  { id: "cost-ice", cost: null },
-  { id: "cost-kebab", cost: null },
-  { id: "cost-milk", cost: null },
-  { id: "cost-muffin", cost: null },
-  { id: "cost-sandwich", cost: null },
-];
-export function updateCostColors() {
-  for (const item of itemsToWatch) {
-    if (item.cost) {
-      updateCostTextColor(item.id, item.cost());
+    const item = itemsToWatch.find((item) => item.id === id);
+    if (item && item.cost) {
+        return item.cost();
     }
-  }
+    return null;
+}
+
+export const itemsToWatch = [
+    {id: "cost-pizza", cost: null},
+    {id: "cost-burger", cost: null},
+    {id: "cost-coffee", cost: null},
+    {id: "cost-dorayaki", cost: null},
+    {id: "cost-fries", cost: null},
+    {id: "cost-HotDog", cost: null},
+    {id: "cost-ice", cost: null},
+    {id: "cost-kebab", cost: null},
+    {id: "cost-milk", cost: null},
+    {id: "cost-muffin", cost: null},
+    {id: "cost-sandwich", cost: null},
+];
+
+export function updateCostColors() {
+    for (const item of itemsToWatch) {
+        if (item.cost) {
+            updateCostTextColor(item.id, item.cost());
+        }
+    }
 }
 
 export function updateCostTextColor(itemID, cost) {
-  const costElement = document.getElementById(itemID);
-  const currentScore = getScore();
+    const costElement = document.getElementById(itemID);
+    const currentScore = getScore();
 
-  if (costElement) {
-    if (currentScore >= cost) {
-      costElement.classList.remove("text-color-accent");
-      costElement.classList.add("text-cost-green");
+    if (costElement) {
+        if (currentScore >= cost) {
+            costElement.classList.remove("text-color-accent");
+            costElement.classList.add("text-cost-green");
+        } else {
+            costElement.classList.add("text-color-accent");
+            costElement.classList.remove("text-cost-green");
+        }
     } else {
-      costElement.classList.add("text-color-accent");
-      costElement.classList.remove("text-cost-green");
+        console.warn("L'élément avec l'ID", itemID, "n'a pas été trouvé.");
     }
-  } else {
-    console.warn("L'élément avec l'ID", itemID, "n'a pas été trouvé.");
-  }
 }
 
 export function autoClicker() {
-  if (autoClickerActive) {
-    const currentScore = getScore();
-    const newScore = currentScore + autoClickValue;
-    updateState({ score: newScore });
-  }
+    if (autoClickerActive) {
+        const currentScore = getScore();
+        const newScore = currentScore + autoClickValue;
+        updateState({score: newScore});
+    }
 }
+
 const clicValueElement = document.getElementById("clic");
 
 function updateClicValueText() {
-  const currentClicValue = getClicValue();
-  clicValueElement.textContent = ` ${currentClicValue}`;
+    const currentClicValue = getClicValue();
+    clicValueElement.textContent = ` ${currentClicValue}`;
 }
 
-export { updateClicValueText };
+export {updateClicValueText};
